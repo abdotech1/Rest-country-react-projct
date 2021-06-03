81ff41ae994957826a4e3744f9655a2c37860f54
abdotech1 <83065866+abdotech1@users.noreply.github.com>
2021-06-03 16:42:35 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Header from './components/Header'
+import Filter from './components/Filter'
+import Countries from './components/Countries'
+
+
+
+function App() {
+  return (
+    <>
+    <Header />
+    
+    <Filter/>
+    <Countries/>
+    </>
+  )
+   
+  
+}
+
+export default App;
